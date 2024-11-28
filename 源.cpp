ed1562904adeb2af90b6c71e8058377ca2db32b5
qxxsh <168195582+qxxsh@users.noreply.github.com>
2024-11-28 16:36:32 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define MVNum 100                 //最大顶点数 
+typedef struct {
+    char vexs[MVNum];           //存放顶点的一维数组 
+    int arcs[MVNum][MVNum];     //邻接矩阵 
+    int vexnum, arcnum;          //图的当前顶点数和边数 
+}MGraph;
+void degree(MGraph G);
+void CreatMGraph(MGraph* G);/* 创建图 */
+int main()
+{
+    MGraph G;
+    CreatMGraph(&G);
+    degree(G);
+    return 0;
+}
+void CreatMGraph(MGraph* G)
+{
+    int i, j, k;
+    scanf("%d%d", &G->vexnum, &G->arcnum);
+    getchar();
+    for (i = 0; i < G->vexnum; i++)
+        scanf("%c", &G->vexs[i]);
+    for (i = 0; i < G->vexnum; i++)
+        for (j = 0; j < G->vexnum; j++)
+            G->arcs[i][j] = 0;
+    for (k = 0; k < G->arcnum; k++)
+    {
+        scanf("%d%d", &i, &j);
+        G->arcs[i][j] = 1;
+        G->arcs[j][i] = 1;
+    }
+}
+
+void degree(MGraph G) {
+    int degree[MVNum] = {0};
+
+    for (int i = 0; i < G.vexnum; i++) {
+        for (int j = 0; j < G.vexnum; j++) {
+            if (G.arcs[i][j] == 1) {
+                degree[i]++;
+            }
+        }
+    }
+}
